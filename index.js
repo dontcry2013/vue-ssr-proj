5c68c2a8f0c39191b3e12046dc9c645ea17faff5
Zac <zach.qin@gmail.com>
2019-01-07 09:49:12 +1100

    use universal code

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 const Vue = require('vue')
 const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer()
+const createApp = require('./app')
 
 server.get('*', (req, res) => {
-  const app = new Vue({
-    data: {
-      url: req.url
-    },
-    template: `<div>The visited URL is: {{ url }}</div>`
-  })
+  const context = { url: req.url }
+  const app = createApp(context)
 
   renderer.renderToString(app, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
     }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+    res.end(html)
   })
 })
 
