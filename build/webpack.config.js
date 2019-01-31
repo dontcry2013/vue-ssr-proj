a6150fc0445e07e629b38f675077dff441fff598
Zac <zach.qin@gmail.com>
2019-01-31 13:55:02 +1100

    try to add hot load(in the half way)

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   output: {
-    path: resolve(__dirname, './dist'),
+    path: resolve(__dirname, '../dist'),
     publicPath: '/dist/',
     filename: '[name].[chunkhash].js'
   },
